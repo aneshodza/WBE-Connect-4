4cc7352c95a1907e03d47fa238162d12139ccec8
Anes Hodza <anes.hodza04@gmail.com>
2025-12-31 14:28:02 +0100

    Add random creation from lab 8

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const state = {
 };
 
 /**
-* Helper to get the value of a specific field on the board.
-* Exists so memorization of which access is row and which
-* is column is centralized.
-* @param {number} row - The row index of the field.
-* @param {number} col - The column index of the field.
-* @returns {string} The value at the specified field ("", "r", "b").
-*/
+ * Helper to get the value of a specific field on the board.
+ * Exists so memorization of which access is row and which
+ * is column is centralized.
+ * @param {number} row - The row index of the field.
+ * @param {number} col - The column index of the field.
+ * @returns {string} The value at the specified field ("", "r", "b").
+ */
 const getField = (row, col) => {
   return state.board[row][col];
 };
@@ -31,17 +31,41 @@ function elt(type, attrs, ...children) {
 
 const showBoard = () => {
   state.app.innerHTML = "";
-  const fields = state.board.map((row, rowIdx) => row.map((col, colIdx) => {
-    let content = '';
-    if (col === "b") {
-      content = elt("div", { class: "disc disc-blue" });
-    } else if (col === "r") {
-      content = elt("div", { class: "disc disc-red" });
-    }
+  const fields = state.board.map((row, rowIdx) =>
+    row.map((col, colIdx) => {
+      let content = "";
+      if (col === "b") {
+        content = elt("div", { class: "disc disc-blue" });
+      } else if (col === "r") {
+        content = elt("div", { class: "disc disc-red" });
+      }
 
-    return elt("div", { class: "field", id: `field-${rowIdx}-${colIdx}`}, content)
-  }))
+      return elt(
+        "div",
+        { class: "field", id: `field-${rowIdx}-${colIdx}` },
+        content,
+      );
+    }),
+  );
 
   const board = elt("div", { class: "board" }, ...fields.flat());
   state.app.appendChild(board);
-}
+};
+
+const fillRandom = () => {
+  const cols = state.board[0].length;
+  const rows = state.board.length;
+
+  const randomCol = Math.floor(Math.random() * cols);
+  const randomRow = Math.floor(Math.random() * rows);
+
+  const colors = ["r", "b", ""];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+  state.board[randomRow][randomCol] = randomColor;
+};
+
+setInterval(() => {
+  fillRandom();
+  showBoard();
+}, 1000);
